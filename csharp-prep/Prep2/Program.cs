f8492263762b83c49081eb51f90b7bb80202a658
HaniaFlores <hanniaflores03@gmail.com>
2023-01-07 10:37:56 -0600

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,98 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        //Ask the user their grade.
+        Console.Write("Insert your grade percentage: ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter;
+        int sign = grade % 10;
+        
+        //Printing the letter grade.
+        if (grade >= 90)
+        {
+            if (sign < 3)
+            {
+                letter = "A-";
+            }
+
+            else
+            {
+                letter = "A";
+            }
+        }
+
+        else if (grade >= 80)
+        {
+            if (sign >= 7)
+            {
+                letter = "B+";
+            }
+
+            else if (sign < 3)
+            {
+                letter = "B-";
+            }
+
+            else
+            {
+                letter = "B";
+            }
+        }
+
+        else if (grade >= 70)
+        {
+            if (sign >= 7)
+            {
+                letter = "C+";
+            }
+
+            else if (sign < 3)
+            {
+                letter = "C-";
+            }
+
+            else
+            {
+                letter = "C";
+            }
+        }
+
+        else if (grade >= 60)
+        {
+            if (sign >= 7)
+            {
+                letter = "D+";
+            }
+
+            else if (sign < 3)
+            {
+                letter = "D-";
+            }
+
+            else
+            {
+                letter = "D";
+            }
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        //Print if you passed or not the course.
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course.");
+        }
+
+        else
+        {
+            Console.WriteLine("Keep trying to get a higher score next time.");
+        }
+
     }
 }
