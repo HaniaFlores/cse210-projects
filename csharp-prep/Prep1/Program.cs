c332e2631a74845e0a4014a8cc06f195b08ea964
HaniaFlores <hanniaflores03@gmail.com>
2023-01-06 21:47:44 -0600

    Finished Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Ask the user for their name.
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        //Print the sentence.
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
+
     }
 }
