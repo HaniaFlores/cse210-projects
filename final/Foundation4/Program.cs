d893d96af89a404199db25ead371256fc891f38f
HaniaFlores <hanniaflores03@gmail.com>
2023-03-29 16:03:05 -0600

    Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -15,16 +15,16 @@ class Program
         activities.Add(act1);
 
         /* Cycling Activity */
-        CyclingActivity act2 = new CyclingActivity(new DateTime(2023, 3, 21), 35, 15);
+        CyclingActivity act2 = new CyclingActivity(new DateTime(2023, 3, 25), 35, 15);
         activities.Add(act2);
 
         /* Swimming Activity */
-        SwimmingActivity act3 = new SwimmingActivity(new DateTime(2023, 3, 29), 40, 35);
+        SwimmingActivity act3 = new SwimmingActivity(new DateTime(2023, 3, 26), 40, 32);
         activities.Add(act3);
 
         foreach (Activity activity in activities)
         {
-            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine("\n" + activity.GetSummary());
         }
     }
 }
